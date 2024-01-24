df14a9bdf70ee9e68564d5193a940a4012fc6831
Sohail Shams <sohail.shams@airelogic.com>
2024-01-24 22:10:33 +0000

    Project setup.

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello Worsssspppp5555555555ppppppppppd!'));
+
+
+
+module.exports = app;
---
+++ listen.js
@@ -0,0 +1,6 @@
+const app = require("./app.js");
+console.log('app')
+
+app.listen(9090, () => {
+    console.log('App listening on port 9090!')
+});
